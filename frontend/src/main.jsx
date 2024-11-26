489fceb0df8c9d08cb386f5d7878cc80c985a6bb
shashankbaheti8 <shashankbaheti8@gmail.com>
2024-11-26 20:16:46 +0530

    frontend_courses

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
+import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const serverURL = "http://localhost:3000"
+export const serverURL = "http://localhost:3000";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserContextProvider>
-      <App />
+      <CourseContextProvider>
+        <App />
+      </CourseContextProvider>
     </UserContextProvider>
   </StrictMode>
 );
